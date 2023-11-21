b7c57c16b4e224ddb1b888deaf8f7bba71305431
Tomasz <tomek15377@gmail.com>
2023-11-21 15:43:48 +0100

    The websied is finish.

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,23 @@
-
-const burgerBtn = document.querySelector(".burger-btn")
-const navMenu = document.querySelector(".nav__items")
-const allNavItems = document.querySelectorAll(".nav__item")
-
-
-
+const burgerBtn = document.querySelector('.burger-btn')
+const navMenu = document.querySelector('.nav__items')
+const allNavItems = document.querySelectorAll('.nav__item')
 
 //open/close bar menu
 const handelMenu = () => {
-  navMenu.classList.toggle('nav__items--active')
+	navMenu.classList.toggle('nav__items--active')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			navMenu.classList.remove('nav__items--active')
 		})
-	})}
-	
-
-
-
-
-
+	})
+}
 
 //footer year
-document.addEventListener("DOMContentLoaded", function() { 
-  const yrSpan = document.querySelector('.footer__year');
-  const footerYear = new Date().getFullYear();
-  yrSpan.textContent = footerYear;
-});
+document.addEventListener('DOMContentLoaded', function () {
+	const yrSpan = document.querySelector('.footer__year')
+	const footerYear = new Date().getFullYear()
+	yrSpan.textContent = footerYear
+})
 
-burgerBtn.addEventListener("click", handelMenu)
+burgerBtn.addEventListener('click', handelMenu)
