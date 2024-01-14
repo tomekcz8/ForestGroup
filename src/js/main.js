84c599bf941c2d0e4436f2cc8cbccb42630cdb54
Tomasz <tomek15377@gmail.com>
2024-01-14 19:58:02 +0100

    zmiany

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,12 @@ const navMenu = document.querySelector('.nav__items')
 const allNavItems = document.querySelectorAll('.nav__item')
 
 //open/close bar menu
-const handelMenu = () => {
+const handelMenu = (e) => {
 	navMenu.classList.toggle('nav__items--active')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			navMenu.classList.remove('nav__items--active')
-
 		})
 	})
 }
